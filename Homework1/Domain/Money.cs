16015894d232f93213673e7612f3b1ee54cb4cb1
Александр Некрасов <zapasoiler@gmail.com>
2023-07-16 15:34:34 +0500

    [Feature] Implement IComparable for Money, add + and –operations, override Equals, GetHashCode, ToString.

--- Homework1/Domain/Money.cs
+++ Homework1/Domain/Money.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Модель для хранения денег
 /// </summary>
-public class Money
+public sealed class Money : IComparable<Money>
 {
 	public Money(int rubles, int kopeks)
 		: this(false, rubles, kopeks)
@@ -27,8 +27,103 @@ public class Money
 	/// </summary>
 	public int Rubles { get; }
 
-	/// <summary>
-	/// Количество копеек
-	/// </summary>
-	public int Kopeks { get; }
+    /// <summary>
+    /// Количество копеек
+    /// </summary>
+    public int Kopeks { get; }
+
+    private const int KopeсksInRuble = 100;
+
+    public int CompareTo(Money? other)
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return 0;
+        }
+
+        if (other is null)
+        {
+            return 1;
+        }
+
+        return ConvertToKopeсks(this).CompareTo(ConvertToKopeсks(other));
+    }
+
+    public static bool operator <(Money? left, Money? right)
+    {
+        return left?.CompareTo(right) < 0;
+    }
+
+    public static bool operator >(Money? left, Money? right)
+    {
+        return left?.CompareTo(right) > 0;
+    }
+
+    public static bool operator <=(Money? left, Money? right)
+    {
+        return left?.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >=(Money? left, Money? right)
+    {
+        return left?.CompareTo(right) >= 0;
+    }
+
+    public static Money operator +(in Money? left, in Money? right)
+    {
+        ArgumentNullException.ThrowIfNull(left);
+        ArgumentNullException.ThrowIfNull(right);
+
+        int kopeсksSum = ConvertToKopeсks(left) + ConvertToKopeсks(right);
+
+        return ConvertToMoney(kopeсksSum);
+    }
+
+    public static Money operator -(in Money? left, in Money? right)
+    {
+        ArgumentNullException.ThrowIfNull(left);
+        ArgumentNullException.ThrowIfNull(right);
+
+        int kopeсksDif = ConvertToKopeсks(left) - ConvertToKopeсks(right);
+
+        return ConvertToMoney(kopeсksDif);
+    }
+
+    private static int ConvertToKopeсks(in Money money)
+    {
+        int kopecks = money.Rubles * KopeсksInRuble + money.Kopeks;
+        if (money.IsNegative)
+        {
+            return -1 * kopecks;
+        }
+
+        return kopecks;
+    }
+
+    private static Money ConvertToMoney(in int kopeсks)
+    {
+        bool isNegative = kopeсks < 0;
+        int unsignedKopeсks = isNegative
+                                  ? -1 * kopeсks
+                                  : kopeсks;
+
+        return new Money(isNegative, unsignedKopeсks / KopeсksInRuble, unsignedKopeсks % KopeсksInRuble);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Money other && this.CompareTo(other) == 0;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.IsNegative, this.Rubles, this.Kopeks);
+    }
+
+    public override string ToString()
+    {
+        return $"{(this.IsNegative
+                       ? "-"
+                       : string.Empty)}{this.Rubles}.{this.Kopeks}";
+    }
 }
