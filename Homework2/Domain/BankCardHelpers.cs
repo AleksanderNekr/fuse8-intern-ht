a6dbd2449e58e74d6ee826694e4f71fb3b03c80c
Александр Некрасов <zapasoiler@gmail.com>
2023-07-19 21:15:51 +0500

    [Feature] Add private field value get in BankCardHelpers.cs.

--- Homework2/Domain/BankCardHelpers.cs
+++ Homework2/Domain/BankCardHelpers.cs
@@ -4,14 +4,17 @@ namespace Fuse8_ByteMinds.SummerSchool.Domain;
 
 public static class BankCardHelpers
 {
-	/// <summary>
-	/// Получает номер карты без маски
-	/// </summary>
-	/// <param name="card">Банковская карта</param>
-	/// <returns>Номер карты без маски</returns>
-	public static string GetUnmaskedCardNumber(BankCard card)
-	{
-		// TODO С помощью рефлексии получить номер карты без маски
-		return card.MaskedCardNumber;
-	}
+    /// <summary>
+    /// Получает номер карты без маски
+    /// </summary>
+    /// <param name="card">Банковская карта</param>
+    /// <returns>Номер карты без маски</returns>
+    public static string GetUnmaskedCardNumber(BankCard card)
+    {
+        return card.GetType()
+                   .GetField("_number", BindingFlags.Instance | BindingFlags.NonPublic)
+                  ?.GetValue(card)
+                  ?.ToString()
+            ?? throw new InvalidOperationException("Не удалось получить номер карты без маски");
+    }
 }
