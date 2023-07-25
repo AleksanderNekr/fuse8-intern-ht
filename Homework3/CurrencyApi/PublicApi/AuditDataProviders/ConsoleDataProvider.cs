f6fe0ff218b3681ab30bec2e9bf1e275fd7cd6f4
Александр Некрасов <zapasoiler@gmail.com>
2023-07-25 21:46:17 +0500

    [Feature] Add swagger config, http logging, audit logging to console, http client.

---
+++ Homework3/CurrencyApi/PublicApi/AuditDataProviders/ConsoleDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using Audit.Core;
+using Newtonsoft.Json;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.AuditDataProviders;
+
+internal sealed class ConsoleDataProvider : AuditDataProvider
+{
+    public override object InsertEvent(AuditEvent auditEvent)
+    {
+        string json = JsonConvert.SerializeObject(auditEvent,
+                                                  new JsonSerializerSettings
+                                                  {
+                                                      ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                                                  });
+
+        Console.WriteLine(json);
+
+        return Guid.NewGuid();
+    }
+}
--- Homework3/CurrencyApi/PublicApi/Startup.cs
+++ Homework3/CurrencyApi/PublicApi/Startup.cs
@@ -1,43 +1,90 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Configuration;
+using System.Text.Json.Serialization;
+using Audit.Http;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.AuditDataProviders;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Settings;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.OpenApi.Models;
 
 namespace Fuse8_ByteMinds.SummerSchool.PublicApi;
 
 public class Startup
 {
-	private readonly IConfiguration _configuration;
+    private readonly IConfiguration _configuration;
 
-	public Startup(IConfiguration configuration)
-	{
-		_configuration = configuration;
-	}
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
-	public void ConfigureServices(IServiceCollection services)
-	{
-		services.AddControllers()
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers()
 
-			// Добавляем глобальные настройки для преобразования Json
-			.AddJsonOptions(
-				options =>
-				{
-					// Добавляем конвертер для енама
-					// По умолчанию енам преобразуется в цифровое значение
-					// Этим конвертером задаем перевод в строковое занчение
-					options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-				});
-		;
-		services.AddEndpointsApiExplorer();
-		services.AddSwaggerGen();
-	}
+                 // Добавляем глобальные настройки для преобразования Json
+                .AddJsonOptions(static options =>
+                                {
+                                    // Добавляем конвертер для енама
+                                    // По умолчанию енам преобразуется в цифровое значение
+                                    // Этим конвертером задаем перевод в строковое занчение
+                                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                                });
 
-	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-	{
-		if (env.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(static options =>
+                               {
+                                   options.SwaggerDoc("v1",
+                                                      new OpenApiInfo
+                                                      {
+                                                          Title       = "API",
+                                                          Version     = "v1",
+                                                          Description = "Test API"
+                                                      });
+                                   options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+                                                                           $"{typeof(Program).Assembly.GetName().Name}.xml"),
+                                                              true);
+                               });
+        services.AddHttpLogging(static options =>
+                                {
+                                    options.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.Response;
+                                    options.MediaTypeOptions.AddText("application/javascript");
+                                });
 
-		app.UseRouting()
-			.UseEndpoints(endpoints => endpoints.MapControllers());
-	}
+        Audit.Core.Configuration.Setup()
+             .UseCustomProvider(new ConsoleDataProvider());
+
+        services.AddHttpClient("DefaultClient",
+                               client =>
+                               {
+                                   string apiKey = this._configuration.GetValue<string>("API_KEY")
+                                                ?? throw new SettingsPropertyNotFoundException("API key not found");
+                                   client.BaseAddress = new Uri("https://api.currencyapi.com/v3/");
+                                   client.DefaultRequestHeaders.Add("apikey", apiKey);
+                               })
+                .AddAuditHandler(static configurator =>
+                                 {
+                                     configurator.IncludeRequestBody()
+                                                 .IncludeRequestHeaders()
+                                                 .IncludeResponseBody()
+                                                 .IncludeResponseHeaders()
+                                                 .IncludeContentHeaders();
+                                 });
+
+        IConfigurationSection currenciesSection = this._configuration.GetRequiredSection("Currencies");
+        services.Configure<CurrenciesSettings>(currenciesSection);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting()
+           .UseEndpoints(static endpoints => endpoints.MapControllers());
+
+        app.UseHttpLogging();
+    }
 }
