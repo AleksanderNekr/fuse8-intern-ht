d500c90f21acdb329704e7e2d981b4bd97370c99
Александр Некрасов <zapasoiler@gmail.com>
2023-07-25 21:45:05 +0500

    [Feature] Add CurrenciesSettings.cs.

---
+++ Homework3/CurrencyApi/PublicApi/Settings/CurrenciesSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Settings;
+
+public sealed class CurrenciesSettings
+{
+    public string DefaultCurrency { get; set; } = null!;
+
+    public string BaseCurrency { get; set; } = null!;
+
+    public int DecimalPlace { get; set; }
+}
