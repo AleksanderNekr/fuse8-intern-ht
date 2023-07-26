01858456124bc1fea40f2ce7921e3ebec011e5fa
Александр Некрасов <zapasoiler@gmail.com>
2023-07-26 12:49:44 +0500

    [Feature] Add external api check.

--- Homework3/CurrencyApi/PublicApi/Controllers/HealthCheckController.cs
+++ Homework3/CurrencyApi/PublicApi/Controllers/HealthCheckController.cs
@@ -9,18 +9,40 @@ namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Controllers;
 [Route("healthcheck")]
 public class HealthCheckController : ControllerBase
 {
-	/// <summary>
-	/// Проверить что API работает
-	/// </summary>
-	/// <param name="checkExternalApi">Необходимо проверить работоспособность внешнего API. Если FALSE или NULL - проверяется работоспособность только текущего API</param>
-	/// <response code="200">
-	/// Возвращает если удалось получить доступ к API
-	/// </response>
-	/// <response code="400">
-	/// Возвращает если удалось не удалось получить доступ к API
-	/// </response>
-	[HttpGet]
-	public HealthCheckResult Check(bool? checkExternalApi) => new() { Status = CheckStatus.Ok, CheckedOn = DateTimeOffset.Now };
+    private readonly HttpClient _externalHttpClient;
+
+    public HealthCheckController(IHttpClientFactory clientFactory)
+    {
+        this._externalHttpClient = clientFactory.CreateClient("DefaultClient");
+    }
+
+    /// <summary>
+    /// Проверить что API работает
+    /// </summary>
+    /// <param name="checkExternalApi">Необходимо проверить работоспособность внешнего API. Если FALSE или NULL - проверяется работоспособность только текущего API</param>
+    /// <param name="stopToken">Токен отмены выполнения.</param>
+    /// <response code="200">
+    /// Возвращает если удалось получить доступ к API
+    /// </response>
+    /// <response code="400">
+    /// Возвращает если удалось не удалось получить доступ к API
+    /// </response>
+    [HttpGet]
+    public async Task<HealthCheckResult> Check(bool? checkExternalApi, CancellationToken stopToken)
+    {
+        if (checkExternalApi is not true)
+        {
+            return new HealthCheckResult { Status = CheckStatus.Ok, CheckedOn = DateTimeOffset.Now };
+        }
+
+        const string        requestUri = "status";
+        HttpResponseMessage response   = await this._externalHttpClient.GetAsync(requestUri, stopToken);
+
+        return response.IsSuccessStatusCode
+                   ? new HealthCheckResult { Status = CheckStatus.Ok, CheckedOn     = DateTimeOffset.Now }
+                   : new HealthCheckResult { Status = CheckStatus.Failed, CheckedOn = DateTimeOffset.Now };
+
+    }
 }
 
 /// <summary>
@@ -28,29 +50,29 @@ public class HealthCheckController : ControllerBase
 /// </summary>
 public record HealthCheckResult
 {
-	/// <summary>
-	/// Дата проверки
-	/// </summary>
-	public DateTimeOffset CheckedOn { get; init; }
+    /// <summary>
+    /// Дата проверки
+    /// </summary>
+    public DateTimeOffset CheckedOn { get; init; }
 
-	/// <summary>
-	/// Статус работоспособности API
-	/// </summary>
-	public CheckStatus Status { get; init; }
+    /// <summary>
+    /// Статус работоспособности API
+    /// </summary>
+    public CheckStatus Status { get; init; }
 
-	/// <summary>
-	/// Статус API
-	/// </summary>
-	public enum CheckStatus
-	{
-		/// <summary>
-		/// API работает
-		/// </summary>
-		Ok = 1,
+    /// <summary>
+    /// Статус API
+    /// </summary>
+    public enum CheckStatus
+    {
+        /// <summary>
+        /// API работает
+        /// </summary>
+        Ok = 1,
 
-		/// <summary>
-		/// Ошибка в работе API
-		/// </summary>
-		Failed = 2,
-	}
+        /// <summary>
+        /// Ошибка в работе API
+        /// </summary>
+        Failed = 2,
+    }
 }
