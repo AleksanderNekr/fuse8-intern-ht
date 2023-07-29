7b6db2bf60dffab3ec2e6c70aab7cdcc48b8fba7
Александр Некрасов <zapasoiler@gmail.com>
2023-07-29 18:15:05 +0500

    [Feature] Add CurrencyApiService.

---
+++ Homework3/CurrencyApi/PublicApi/Services/CurrencyApiService.cs
@@ -0,0 +1,124 @@
+﻿using System.Net;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Exceptions;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Services;
+
+/// <inheritdoc />
+public sealed class CurrencyApiService : ICurrencyApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public CurrencyApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    /// <inheritdoc />
+    public async Task<CurrencyInfo> GetCurrencyInfoAsync(string            currency,
+                                                         string            baseCurrency,
+                                                         int               decimalPlace,
+                                                         CancellationToken stopToken)
+    {
+        await CheckRequestsLimitAsync(stopToken);
+
+        var requestUri = $"latest?currencies={currency}&base_currency={baseCurrency}";
+
+        HttpResponseMessage response = await _httpClient.GetAsync(requestUri, stopToken);
+        if (response.StatusCode == HttpStatusCode.UnprocessableEntity)
+        {
+            throw new CurrencyNotFoundException(nameof(currency), currency);
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        string  responseBody = await response.Content.ReadAsStringAsync(stopToken);
+        decimal value        = GetCurrencyFromResponse(currency, responseBody);
+        decimal roundedValue = Math.Round(value, decimalPlace);
+
+        return new CurrencyInfo
+               {
+                   Code  = currency,
+                   Value = roundedValue,
+               };
+    }
+
+    /// <inheritdoc />
+    public async Task<CurrencyOnDateInfo> GetCurrencyInfoOnDateAsync(string            currency,
+                                                                     string            baseCurrency,
+                                                                     int               decimalPlace,
+                                                                     DateOnly          date,
+                                                                     CancellationToken stopToken)
+    {
+        await CheckRequestsLimitAsync(stopToken);
+
+        var requestUri = $"historical?currencies={currency}&base_currency={baseCurrency}&&date={date}";
+
+        HttpResponseMessage response = await _httpClient.GetAsync(requestUri, stopToken);
+        if (response.StatusCode == HttpStatusCode.UnprocessableEntity)
+        {
+            throw new CurrencyNotFoundException(nameof(currency), currency);
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        string  responseBody = await response.Content.ReadAsStringAsync(stopToken);
+        decimal value        = GetCurrencyFromResponse(currency, responseBody);
+        decimal roundedValue = Math.Round(value, decimalPlace);
+
+        return new CurrencyOnDateInfo
+               {
+                   Date  = date,
+                   Code  = currency,
+                   Value = roundedValue,
+               };
+    }
+
+    /// <inheritdoc />
+    public async Task<MonthSection> GetMonthSectionAsync(CancellationToken stopToken)
+    {
+        const string        requestUri = "status";
+        HttpResponseMessage response   = await _httpClient.GetAsync(requestUri, stopToken);
+
+        response.EnsureSuccessStatusCode();
+
+        string  responseBody   = await response.Content.ReadAsStringAsync(stopToken);
+        JObject responseParsed = JObject.Parse(responseBody);
+        var     quotasSection  = responseParsed.GetValue("quotas")!.ToObject<JObject>()!;
+        var     monthSection   = quotasSection.GetValue("month")!.ToObject<MonthSection>();
+
+        return monthSection;
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> IsConnectedAsync(CancellationToken stopToken)
+    {
+        const string        requestUri = "status";
+        HttpResponseMessage response   = await _httpClient.GetAsync(requestUri, stopToken);
+
+        return response.IsSuccessStatusCode;
+    }
+
+    private static decimal GetCurrencyFromResponse(string currency, string responseBody)
+    {
+        JObject responseParsed  = JObject.Parse(responseBody);
+        var     dataSection     = responseParsed.GetValue("data")!.ToObject<JObject>()!;
+        var     currencySection = dataSection.GetValue(currency)!.ToObject<JObject>()!;
+        var     value           = currencySection.GetValue("value")!.ToObject<decimal>();
+
+        return value;
+    }
+
+    private async Task CheckRequestsLimitAsync(CancellationToken stopToken)
+    {
+        MonthSection monthSection = await GetMonthSectionAsync(stopToken);
+
+        if(monthSection.Remaining > 0)
+        {
+            return;
+        }
+
+        throw new ApiRequestLimitException("API requests limit exceeded!");
+    }
+}
---
+++ Homework3/CurrencyApi/PublicApi/Services/ICurrencyApiService.cs
@@ -0,0 +1,61 @@
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Exceptions;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Models;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Services;
+
+/// <summary>
+///     Сервис получения данных от CurrencyApi.
+/// </summary>
+public interface ICurrencyApiService
+{
+    /// <summary>
+    ///     Получение информации о валюте относительно базовой.
+    /// </summary>
+    /// <param name="currency">Валюта.</param>
+    /// <param name="baseCurrency">Базовая валюта.</param>
+    /// <param name="decimalPlace">Количество знаков после запятой.</param>
+    /// <param name="stopToken">Токен отмены операции.</param>
+    /// <returns> Объект типа <see cref="CurrencyInfo" />. </returns>
+    /// <exception cref="ApiRequestLimitException">Превышен лимит запросов к API.</exception>
+    /// <exception cref="CurrencyNotFoundException">Не найдена валюта.</exception>
+    /// <exception cref="HttpRequestException">HTTP-ответ был не успешен.</exception>
+    public Task<CurrencyInfo> GetCurrencyInfoAsync(string            currency,
+                                                         string            baseCurrency,
+                                                         int               decimalPlace,
+                                                         CancellationToken stopToken);
+
+    /// <summary>
+    ///     Получение информации о валюте относительно базовой на определенную дату.
+    /// </summary>
+    /// <param name="currency">Валюта.</param>
+    /// <param name="baseCurrency">Базовая валюта.</param>
+    /// <param name="decimalPlace">Количество знаков после запятой.</param>
+    /// <param name="date">Дата, на которую получена информация.</param>
+    /// <param name="stopToken">Токен отмены операции.</param>
+    /// <returns>Объект типа <see cref="CurrencyOnDateInfo" />.</returns>
+    /// <exception cref="ApiRequestLimitException">Превышен лимит запросов к API.</exception>
+    /// <exception cref="CurrencyNotFoundException">Не найдена валюта.</exception>
+    /// <exception cref="HttpRequestException">HTTP-ответ был не успешен.</exception>
+    public Task<CurrencyOnDateInfo> GetCurrencyInfoOnDateAsync(string            currency,
+                                                               string            baseCurrency,
+                                                               int               decimalPlace,
+                                                               DateOnly          date,
+                                                               CancellationToken stopToken);
+
+    /// <summary>
+    ///     Получении секции месяца в информации об использованных запросах к внешнему API.
+    /// </summary>
+    /// <param name="stopToken">Токен отмены операции.</param>
+    /// <returns>
+    ///     Модель секции месяца <see cref="MonthSection" />.
+    /// </returns>
+    /// <exception cref="HttpRequestException">HTTP-ответ был не успешен.</exception>
+    public Task<MonthSection> GetMonthSectionAsync(CancellationToken stopToken);
+
+    /// <summary>
+    /// Проверяет, доступен ли сервер API.
+    /// </summary>
+    /// <param name="stopToken">Токен отмены операции.</param>
+    /// <returns>true – удалось подключиться к серверу, false – не удалось.</returns>
+    Task<bool> IsConnectedAsync(CancellationToken stopToken);
+}
