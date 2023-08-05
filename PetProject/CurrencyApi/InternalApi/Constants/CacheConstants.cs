ae9870e4025d47005a474c6db20bb0b01560d6d2
Александр Некрасов <zapasoiler@gmail.com>
2023-08-05 17:26:30 +0500

    [Feature] Add CacheConstants.cs.

---
+++ PetProject/CurrencyApi/InternalApi/Constants/CacheConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Constants;
+
+internal static class CacheConstants
+{
+    public const string CacheFolderName = "Cache";
+
+    public const string FilesSearchPattern = "*.json";
+}
