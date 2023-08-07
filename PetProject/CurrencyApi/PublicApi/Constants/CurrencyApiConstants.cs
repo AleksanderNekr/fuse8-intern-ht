7a1772ae943fed1c18dc53b74ed6700d3379a742
Александр Некрасов <zapasoiler@gmail.com>
2023-08-07 13:52:54 +0500

    [Refactor] Remove unnecessary constants in CurrencyApiConstants.cs.

--- PetProject/CurrencyApi/PublicApi/Constants/CurrencyApiConstants.cs
+++ PetProject/CurrencyApi/PublicApi/Constants/CurrencyApiConstants.cs
@@ -2,11 +2,7 @@
 
 internal static class CurrencyApiConstants
 {
-    public const string ApiSettingsKey = "API_KEY";
-
     public const string CurrenciesSectionName = "Currencies";
-
-    public const string ApiKeyHeader = "apikey";
 
     public const string GrpcAddressSettingsKey = "GrpcAddress";
 }
