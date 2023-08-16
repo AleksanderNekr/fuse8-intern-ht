38191b3fc95ccbfd1ee45a62aac8baab2c61edf6
Александр Некрасов <zapasoiler@gmail.com>
2023-08-16 13:53:23 +0500

    [Refactor] Remove unnecessary entities attributes.

--- PetProject/CurrencyApi/InternalApi/Data/Entities/CurrencyInfoEntity.cs
+++ PetProject/CurrencyApi/InternalApi/Data/Entities/CurrencyInfoEntity.cs
@@ -7,12 +7,8 @@ namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
 
 public class CurrencyInfoEntity
 {
-    private const int MatchTimeoutInMilliseconds = 1000;
-
-    [RegularExpression("^[a-zA-Z]{3}$", MatchTimeoutInMilliseconds = MatchTimeoutInMilliseconds)]
     public CurrencyType Code { get; set; }
 
-    [Range(0, (double)decimal.MaxValue, ConvertValueInInvariantCulture = true)]
     public decimal Value { get; set; }
 
     // Nav property.
