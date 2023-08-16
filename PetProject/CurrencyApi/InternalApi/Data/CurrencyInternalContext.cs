3dc580c09a20d4ba81d0cd45fde998865b1b1967
Александр Некрасов <zapasoiler@gmail.com>
2023-08-16 13:18:50 +0500

    [Refactor] Move CurrencyInternalContext.cs to Data folder.

---
+++ PetProject/CurrencyApi/InternalApi/Data/CurrencyInternalContext.cs
@@ -0,0 +1,31 @@
+﻿using Fuse8_ByteMinds.SummerSchool.InternalApi.Constants;
+using Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Data;
+
+/// <inheritdoc />
+public class CurrencyInternalContext : DbContext
+{
+    /// <inheritdoc />
+    public CurrencyInternalContext(DbContextOptions<CurrencyInternalContext> options) : base(options)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(CurrencyApiConstants.SchemaName);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+
+    /// <summary>
+    /// Коллекция сущностей валют на определенную дату.
+    /// </summary>
+    public DbSet<CurrenciesOnDateEntity> CurrenciesOnDates { get; set; } = null!;
+
+    /// <summary>
+    /// Коллекция сущностей валют.
+    /// </summary>
+    public DbSet<CurrencyInfoEntity> CurrencyInfos { get; set; } = null!;
+}
