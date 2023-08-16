fe4dfc82ef05f51f3c5c7d013a79a3f58eb77a26
Александр Некрасов <zapasoiler@gmail.com>
2023-08-16 13:19:14 +0500

    [Feature] Add entities config.

---
+++ PetProject/CurrencyApi/InternalApi/Data/EntitiesConfigurations/CurrenciesOnDateConfig.cs
@@ -0,0 +1,24 @@
+﻿using Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Data.EntitiesConfigurations;
+
+public class CurrenciesOnDateConfig : IEntityTypeConfiguration<CurrenciesOnDateEntity>
+{
+    public void Configure(EntityTypeBuilder<CurrenciesOnDateEntity> builder)
+    {
+        builder.Property(static currencies => currencies.LastUpdatedAt)
+               .HasColumnName("last_updated_at")
+               .HasColumnType("timestamptz");
+
+        builder.HasKey(static currencies => currencies.LastUpdatedAt)
+               .HasName("cur_on_date_pk");
+
+        builder.ToTable("currencies_on_date",
+                        static tableBuilder => tableBuilder.HasCheckConstraint("date_range_ch",
+                                                                               "last_updated_at <= now()"));
+    }
+}
---
+++ PetProject/CurrencyApi/InternalApi/Data/EntitiesConfigurations/CurrencyInfoConfig.cs
@@ -0,0 +1,31 @@
+﻿using Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
+using Fuse8_ByteMinds.SummerSchool.InternalApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Data.EntitiesConfigurations;
+
+public class CurrencyInfoConfig : IEntityTypeConfiguration<CurrencyInfoEntity>
+{
+    public void Configure(EntityTypeBuilder<CurrencyInfoEntity> builder)
+    {
+        builder.Property(static entity => entity.Code)
+               .HasConversion(static @enum => @enum.ToString(),
+                              static value => Enum.Parse<CurrencyType>(value, true))
+               .HasColumnName("code")
+               .HasColumnType("varchar");
+
+        builder.Property(static entity => entity.Value)
+               .HasColumnName("value")
+               .HasColumnType("decimal");
+
+        builder.HasOne(static currInfo => currInfo.CurrenciesOnDate)
+               .WithMany(static currOnDate => currOnDate.Currencies)
+               .HasForeignKey(static currInfo => currInfo.UpdatedAt)
+               .HasConstraintName("currencies_on_date_have_infos_fk");
+
+        builder.ToTable("currency_info");
+    }
+}
