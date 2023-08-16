7f9722417d048adf27efb5d0ef5489982502e046
Александр Некрасов <zapasoiler@gmail.com>
2023-08-16 13:18:28 +0500

    [Feature] Add CurrenciesOnDateEntity.cs and CurrencyInfoEntity.cs.

---
+++ PetProject/CurrencyApi/InternalApi/Data/Entities/CurrencyInfoEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Fuse8_ByteMinds.SummerSchool.InternalApi.Models;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
+
+public class CurrencyInfoEntity
+{
+    private const int MatchTimeoutInMilliseconds = 1000;
+
+    [RegularExpression("^[a-zA-Z]{3}$", MatchTimeoutInMilliseconds = MatchTimeoutInMilliseconds)]
+    public CurrencyType Code { get; set; }
+
+    [Range(0, (double)decimal.MaxValue, ConvertValueInInvariantCulture = true)]
+    public decimal Value { get; set; }
+
+    // Nav property.
+    public DateTime UpdatedAt { get; set; }
+
+    // Nav property.
+    public CurrenciesOnDateEntity CurrenciesOnDate { get; set; } = null!;
+}
