6ef1f26ddc41feb2b7907572b9cd90e080eaa58a
Александр Некрасов <zapasoiler@gmail.com>
2023-08-18 13:05:53 +0500

    [Feature] Add settings currency constraint for values.

---
+++ PetProject/CurrencyApi/PublicApi/Data/Migrations/20230818074024_Add_currency_check_for_default.cs
@@ -0,0 +1,30 @@
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Constants;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_currency_check_for_default : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 alter table "user".settings
+                                 add constraint "currency_range_ch"
+                                 check ( default_currency IN ('USD', 'RUB', 'KZT', 'EUR') );
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 alter table "user".settings
+                                 drop constraint "currency_range_ch";
+                                 """);
+        }
+    }
+}
