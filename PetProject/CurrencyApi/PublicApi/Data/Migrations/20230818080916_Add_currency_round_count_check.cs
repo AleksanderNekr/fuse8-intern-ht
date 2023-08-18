c35eb3d0468b698e7e362b0e70d70a7ce53771a9
Александр Некрасов <zapasoiler@gmail.com>
2023-08-18 13:12:46 +0500

    [Feature] Add currency round count range check constraint.

---
+++ PetProject/CurrencyApi/PublicApi/Data/Migrations/20230818080916_Add_currency_round_count_check.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_currency_round_count_check : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 alter table "user".settings
+                                 add constraint "currency_round_count_range_ch"
+                                 check ( currency_round_count >= 0 and currency_round_count <= 28 );
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 alter table "user".settings
+                                 drop constraint "currency_round_count_range_ch";
+                                 """);
+        }
+    }
+}
